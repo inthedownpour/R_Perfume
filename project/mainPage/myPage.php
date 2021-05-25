d78075c089fe66acae888bfdbf009ee1016de900
baeleekang <50096266+baeleekang@users.noreply.github.com>
2021-05-25 13:43:48 +0900

    logoutetc2

--- project/mainPage/myPage.php
+++ project/mainPage/myPage.php
@@ -295,7 +295,7 @@ session_start();?>
                   <li><a href="#">SERIES</a></li>
                 </ul>
               </li>
-              <li class="menu"><a href="" style="color: white;">MY PAGE</a></li>
+              <li class="menu"><a href="choosePage.php" style="color: white;">MY PAGE</a></li>
               <li class="menu"><a href="" class="glyphicon glyphicon-heart" style="color: white;"></a></li>
             </ul>
           </div>
