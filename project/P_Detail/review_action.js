c4d689048dfe86e366aeb4a34bf05f5a1a007218
inthedownpour <simjiyeon@naver.com>
2021-06-14 23:13:14 +0900

    add exp

--- project/P_Detail/review_action.js
+++ project/P_Detail/review_action.js
@@ -10,23 +10,20 @@ function submitReview(){
   if(newreviewEL){
     //class가 review_content인 div만듬
     const contentEL=document.createElement("div");
+
     //문자열 전달
     contentEL.innerText= newreviewEL;
-    contentEL.classList.add('review_row');
+    contentEL.classList.add('review_content');
     document.getElementById('write_coment').appendChild(contentEL);
-/*
-    const contentEL=document.createElement("div");
-              //문자열 전달
-    contentEL.innerText= newreviewEL;
-    contentEL.classList.add('review_row');
-    document.getElementById('write_coment').appendChild(contentEL);
-*/
+
     const dateEL=document.createElement("div");
     const today =new Date();
     const dateString=today.toLocaleString();
     dateEL.classList.add('review_date');
     dateEL.innerText=dateString;
-    document.getElementById('dateset').appendChild(dateEL);
+    document.getElementById('write_coment').appendChild(dateEL);
+
+
   }
   else {
     warnempty();
